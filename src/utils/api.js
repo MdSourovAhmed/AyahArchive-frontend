c6d7a073c1d7acf9f802cc84e56999ac5f13bc9f
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-08-27 23:32:08 +0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,3 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:3000/api', // Update with your backend URL
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
 import axios from 'axios';
 
 const api = axios.create({
