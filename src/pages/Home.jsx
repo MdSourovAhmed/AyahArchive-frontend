ba8f7c8499e974e50baed2a9afc67584dab0a5fc
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-09-04 21:11:44 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,59 +6,6 @@ function Home() {
   const { user } = useContext(AuthContext);
 
   return (
-    // <div className="flex flex-col items-center justify-center p-4 min-h-[calc(100vh-4rem)]">
-    //   <div className="bg-white/80 dark:bg-gray-800/80 p-6 rounded-lg shadow-lg text-center">
-    //     <h1 className="text-5xl font-bold text-teal-500 font-arabic" dir="rtl">
-    //       السلام عليكم
-    //     </h1>
-    //     <h1 className="text-3xl font-bold text-teal-500 mb-4">
-    //       Welcome to AyahArchive
-    //     </h1>
-    //     <p className="text-lg text-gray-600 dark:text-gray-200 mb-8">
-    //       Memorize and Practice The QURA'N with ease.
-    //     </p>
-    //     <Link
-    //       to={user ? "/verses" : "/register"}
-    //       className="bg-teal-500 text-white px-6 py-3 rounded-lg hover:bg-teal-600"
-    //     >
-    //       Get Started
-    //     </Link>
-    //     <div className="mt-12 grid md:grid-cols-3 gap-8">
-    //       <Link
-    //         to="/verses"
-    //         className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow"
-    //       >
-    //         <h3 className="text-xl font-semibold text-teal-500">Add Verse</h3>
-    //         <p className="text-gray-600 dark:text-gray-300">
-    //           Save and organize your favorite SURAHs.
-    //         </p>
-    //       </Link>
-    //       <Link
-    //         to="/quiz"
-    //         className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow"
-    //       >
-    //         <h3 className="text-xl font-semibold text-teal-500">
-    //           Practice Quiz
-    //         </h3>
-    //         <p className="text-gray-600 dark:text-gray-300">
-    //           Test your memory with interactive quizzes.
-    //         </p>
-    //       </Link>
-    //       <Link
-    //         to="/dashboard"
-    //         className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow"
-    //       >
-    //         <h3 className="text-xl font-semibold text-teal-500">
-    //           Track Progress
-    //         </h3>
-    //         <p className="text-gray-600 dark:text-gray-300">
-    //           Monitor your memorization journey.
-    //         </p>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="text-center p-6">
       <h1
         className="text-5xl font-bold text-teal-500 font-arabic p-6"
